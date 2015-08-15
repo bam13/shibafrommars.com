69bbe857edc846a53b8deb12b86d7d0485109e37
bam13 <a.berry993@gmail.com>
2015-08-15 14:17:49 +0200

    jquery add class to evry box

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
 
    var $this=$(this);
 
+   $('.page-call').on('click',function(){
+   $('.page').addClass("content");
+  });
 
-
-$(".page-call").on("click", function () {
-    $(".page").removeClass("content");
-    $(this).addClass("content");
-});
 
    $('.fa-times').on('click',function(){
    $('.content').fadeOut('slow','swing');
